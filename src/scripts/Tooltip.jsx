a1e4abe3ad98c4beee0a3eccfe1a294c6361e08b
niels <niels@kreativsoftware.com>
2016-12-16 10:29:53 +0100

    test

--- src/scripts/Tooltip.jsx
+++ src/scripts/Tooltip.jsx
@@ -127,7 +127,7 @@ export default class JoyrideTooltip extends React.Component {
     };
 
     styles.hole = {
-      top: Math.round((opts.rect.top - document.body.getBoundingClientRect().top) - holePadding),
+      top: Math.round((opts.rect.top - document.querySelector('.joyride').getBoundingClientRect().top) - holePadding),
       left: Math.round(opts.rect.left - holePadding),
       width: Math.round(opts.rect.width + (holePadding * 2)),
       height: Math.round(opts.rect.height + (holePadding * 2))
